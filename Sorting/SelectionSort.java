1e3ddc26da28d3cae87e1c854fbd72df5f33c526
Shaik Zubair <shaikzubair683@gmail.com>
2025-10-28 23:50:09 -0400

    new

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -2,5 +2,6 @@ public class SelectionSort {
     public static void main(String[] args) {
         System.out.print("Hello ");
         System.out.print("Hello ");
+        // nkjn
     }
 }
--- progress_log.md
+++ progress_log.md
@@ -1,5 +1,7 @@
 # Progress Log
 
 ### Day 1 – Oct 28, 2025
+
 ✅ Started DSA Practice
 
+### Day 1 – Oct 29, 2025
