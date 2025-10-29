94727011f6a16e0bd16651f195778d572ad29ec6
Shaik Zubair <shaikzubair683@gmail.com>
2025-10-29 19:15:23 -0400

    Selection Sort

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,7 +1,24 @@
+import java.util.Arrays;
+
 public class SelectionSort {
     public static void main(String[] args) {
-        System.out.print("Hello ");
-        System.out.print("Hello ");
-        // nkjn
+        int[] arr = new int[] { 1, 4, 9, 3, 2, 5 };
+        int[] finalArr = selectionSort(arr);
+        System.out.println("Final string: " + Arrays.toString(finalArr));
+    }
+
+    public static int[] selectionSort(int[] arr) {
+        for (int i = 0; i <= arr.length - 2; i++) {
+            int maxIndex = i;
+            for (int j = i; j <= arr.length - 1; j++) {
+                if (arr[j] > arr[maxIndex]) {
+                    maxIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[maxIndex];
+            arr[maxIndex] = temp;
+        }
+        return arr;
     }
 }
